8baabc3171b64511b08c1c74df405b718a79cc74
marocchino <marocchino@users.noreply.github.com>
2021-04-20 12:01:23 +0900

    🚧 Initial Commit

---
+++ lib/list_to_csv.ex
@@ -0,0 +1,18 @@
+defmodule ListToCsv do
+  @moduledoc """
+  Documentation for `ListToCsv`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ListToCsv.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/list_to_csv_test.exs
@@ -0,0 +1,8 @@
+defmodule ListToCsvTest do
+  use ExUnit.Case
+  doctest ListToCsv
+
+  test "greets the world" do
+    assert ListToCsv.hello() == :world
+  end
+end
